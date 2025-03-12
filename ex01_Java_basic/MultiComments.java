8530f5fa69a5da43018c9325a8d7aa2941955079
Ashutosh <64901146+Ashutosh-saini@users.noreply.github.com>
2025-03-12 20:54:05 +0530

    Add files via upload

---
+++ ex01_Java_basic/MultiComments.java
@@ -0,0 +1,12 @@
+package ex01_Java_basic;
+
+public class MultiComments {
+    public static void main(String[] args) {
+        /*
+        this is multiple comments
+        whatever writing here
+        is commented.
+         */
+        System.out.println("multi comment above");
+    }
+}
