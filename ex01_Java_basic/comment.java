3a7bdf4def0a60aaed2b2d7910448928c072858a
Ashutosh <64901146+Ashutosh-saini@users.noreply.github.com>
2025-03-12 20:51:50 +0530

    Add files via upload

---
+++ ex01_Java_basic/comment.java
@@ -0,0 +1,8 @@
+package ex01_Java_basic;
+
+public class comment {
+    public static void main(String[] args) {
+        // I am a single comment
+        System.out.println("Single comment");
+    }
+}
